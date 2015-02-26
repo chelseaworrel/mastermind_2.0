bf09e56909f5731153b509e973e4130e884925f4
Chelsea Worrel <chelseaworrel@gmail.com>
2015-02-26 14:29:03 -0700

    Created new version of game: Mastermind, created class Timer

---
+++ mastermind_2.0.rb
@@ -0,0 +1,39 @@
+class Timer
+attr_reader :start, :finish
+
+  def initialize
+   @start = start
+   @finish = finish
+
+  end
+
+  def start
+    @start = Time.now #Game
+  end
+
+  def finish
+    @finish = Time.now #Game
+  end
+
+  def elapsed #Printer 
+   elapsed = finish.min - start.min
+
+  end
+
+  def start_minutes
+    start_timer_minutes = start.min
+  end
+
+  def finish_minutes
+    finish_timer_minutes = finish.min
+  end
+
+
+
+end
+
+
+timer = Timer.new
+puts timer.start_minutes
+puts timer.finish_minutes
+puts timer.elapsed
---
+++ mastermind_test_2.0.rb
@@ -0,0 +1,49 @@
+gem 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative 'mastermind_2.0'
+
+# class TimerTest  < Minitest::Test
+#   def test_the_time_exsists
+#     assert Timer.new
+#   end
+#
+#   def test_time_starts
+#     timer = Timer.new
+#     assert_equal 0, timer.start
+#   end
+#
+#   def test_time_starts_at_1
+#     timer = Timer.new
+#     assert_equal 1, timer.start
+#   end
+#
+#   def test_time_starts_at_1000
+#     timer = Timer.new
+#     assert_equal 1000, timer.start
+#   end
+#
+#   def test_time_ends_at_one
+#     timer = Timer.new
+#     assert_equal 1, timer.finish
+#   end
+#
+#
+#   def test_time_ends_at_two
+#     timer = Timer.new
+#     assert_equal 2, timer.finish
+#   end
+#
+#   def test_time_ends_at_1000
+#     timer = Timer.new
+#     assert_equal 1000, timer.finish
+#   end
+#
+#   def time_can_start_and_end_at_the_same_time
+#     timer = Timer.new
+#     assert_equal 0, timer.start
+#   end
+
+
+
+end
